2803976b3956e9a3ade5cee09d773a386c9022bd
abhaysachan01 <abhaysachan360@gmail.com>
2026-02-03 18:57:35 +0530

    home page issue resolved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -47,7 +47,7 @@ const Home = () => {
         {/* Featured Items Grid */}
         <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-8">
           {featuredProducts.map(product => (
-            <ProductCard key={product.id} product={product} />
+            <ProductCard key={product.id} product={product} basePath="/product" />
           ))}
         </div>
       </section>
